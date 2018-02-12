e1fdcc84f64052a4ee9d163ff915729a011d05ac
kvacquier <kvacquier@gmail.com>
2018-02-12 10:09:04 +0100

    Carthage Compatibility

---
+++ SwiftQRScanner/SwiftQRScanner.h
@@ -0,0 +1,19 @@
+//
+//  SwiftQRScanner.h
+//  SwiftQRScanner
+//
+//  Created by Kevin VACQUIER on 12/02/2018.
+//  Copyright © 2018 Kevin VACQUIER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftQRScanner.
+FOUNDATION_EXPORT double SwiftQRScannerVersionNumber;
+
+//! Project version string for SwiftQRScanner.
+FOUNDATION_EXPORT const unsigned char SwiftQRScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftQRScanner/PublicHeader.h>
+
+
